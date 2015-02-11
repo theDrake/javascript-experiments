8010be432c89642352eb480f3c1345814e70bb98
David C. Drake <dcdrake42@gmail.com>
2015-02-11 16:46:25 -0700

    Add "dragon_slayer.js"

---
+++ js/dragon_slayer.js
@@ -0,0 +1,28 @@
+var slaying = true;
+var youHit = Math.floor(Math.random());
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+
+while (slaying)
+{
+  if (youHit)
+  {
+    console.log("You hit the dragon for " + damageThisRound + " damage!");
+    totalDamage += damageThisRound;
+    if (totalDamage >= 4)
+    {
+      console.log("The dragon has been slain! Huzzah!");
+      slaying = false;
+    }
+    else
+    {
+      youHit = Math.floor(Math.random());
+      damageThisRound = Math.floor(Math.random() * 5 + 1);
+    }
+  }
+  else
+  {
+    console.log("Alas, the dragon has defeated you.");
+    slaying = false;
+  }
+}

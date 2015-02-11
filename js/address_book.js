d191a4066483ca6495d90d3095274133e8c9e7ff
David C. Drake <dcdrake42@gmail.com>
2015-02-11 16:47:04 -0700

    Add "address_book.js"

---
+++ js/address_book.js
@@ -0,0 +1,47 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+}
+
+function search(lastName) {
+    for (var i = 0; i < contacts.length; ++i) {
+        if (contacts[i].lastName == lastName)
+        {
+            printPerson(contacts[i]);
+        }
+    }
+}
+
+function add(firstName, lastName, email, phoneNumber) {
+    contacts[contacts.length] = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber
+    };
+}
+
+add("David", "Smith", "blah@blah.blah", "555-5555");
+list();

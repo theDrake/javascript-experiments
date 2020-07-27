3532308bc85b4cd98d7555e61766d1a8c2763900
David C. Drake <theDrake@users.noreply.github.com>
2020-07-27 12:47:36 +0800

    Minor update

--- js/address_book.js
+++ js/address_book.js
@@ -1,45 +1,45 @@
 var bob = {
-    firstName: "Bob",
-    lastName: "Jones",
-    phoneNumber: "(650) 777-7777",
-    email: "bob.jones@example.com"
+  firstName: "Bob",
+  lastName: "Jones",
+  phoneNumber: "(650) 777-7777",
+  email: "bob.jones@example.com"
 };
 
 var mary = {
-    firstName: "Mary",
-    lastName: "Johnson",
-    phoneNumber: "(650) 888-8888",
-    email: "mary.johnson@example.com"
+  firstName: "Mary",
+  lastName: "Johnson",
+  phoneNumber: "(650) 888-8888",
+  email: "mary.johnson@example.com"
 };
 
 var contacts = [bob, mary];
 
 function printPerson(person) {
-    console.log(person.firstName + " " + person.lastName);
+  console.log(person.firstName + " " + person.lastName);
 }
 
 function list() {
-    var contactsLength = contacts.length;
-    for (var i = 0; i < contactsLength; i++) {
-        printPerson(contacts[i]);
-    }
+  var contactsLength = contacts.length;
+  for (var i = 0; i < contactsLength; i++) {
+    printPerson(contacts[i]);
+  }
 }
 
 function search(lastName) {
-    for (var i = 0; i < contacts.length; ++i) {
-        if (contacts[i].lastName == lastName) {
-            printPerson(contacts[i]);
-        }
+  for (var i = 0; i < contacts.length; ++i) {
+    if (contacts[i].lastName == lastName) {
+      printPerson(contacts[i]);
     }
+  }
 }
 
 function add(firstName, lastName, email, phoneNumber) {
-    contacts[contacts.length] = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phoneNumber: phoneNumber
-    };
+  contacts[contacts.length] = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber
+  };
 }
 
 add("David", "Smith", "blah@blah.blah", "555-5555");
